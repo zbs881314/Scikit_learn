63ad86a99b5257df91529cee062d3ea23d21f568
bob <shibo.zhou14@gmail.com>
2018-11-29 20:05:30 -0500

    Update

--- cross_validation.py
+++ cross_validation.py
@@ -1,7 +1,9 @@
-fromm sklearn.datasets import load_iris
+from __future__ import print_function
+from sklearn.datasets import load_iris
 from sklearn.cross_validation import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.cross_validation import cross_val_score
+import matplotlib.pyplot as plt
 
 iris = load_iris()
 X = iris.data
@@ -10,25 +12,23 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=4)
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
-##y_pred = knn.predict(X_test)
+y_pred = knn.predict(X_test)
 print(knn.score(X_test, y_test))
 
-from sklearn.cross_validation import cross_val_score
+
 knn = KNeighborsClassifier(n_neighbors=5)
-scores = cross_val_score(knn, X, y, cv=5, score='accuracy')
-print(scores.mean())
+scores = cross_val_score(knn, X, y, cv=5, scoring='accuracy')
+print(scores)
 
 
-from sklearn.cross_validation import cross_val_score
-import matplotlib.pyplot as plt
+
 k_range = range(1, 31)
 k_scores = []
 for k in k_range:
   knn = KNeighborsClassifier(n_neighbors=k)
   scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy')   # for classification
-  loss = -cross_val_score(knn, X, y, cv=10, scoring='mean_squared_error')  # for regression
+  #loss = -cross_val_score(knn, X, y, cv=10, scoring='mean_squared_error')  # for regression
   k_scores.append(scores.mean())
-  k_scores.append(loss.mean())
   
   
 plt.plot(k_range, k_scores)
